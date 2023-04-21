8b1795ed1af498d08b02a414013bf000ad6e5f9f
sikindra <yadavsikindra4078@gmail.com>
2023-04-21 21:44:28 +0530

    java day 4 solution

---
+++ src/day4/PackageTwo/Executor.java
@@ -0,0 +1,24 @@
+package day4.PackageTwo;
+
+import day4.PackageOne.BaseClass;
+
+public class Executor {
+    public static void main(String[] args) {
+        BaseClass b = new BaseClass();
+
+
+//        b.methodDefault();// can not access the default method from another package
+        b.callMethod();
+        b.methodPublic();
+//        b.methodPrivate();// can not access the private method from another package
+
+//        b.varDefault = 11;
+//        b.varProtected=22;//can not access the protected method from another package
+        b.varPublic = 33;
+
+//        b.methodDefault();//can not access the default method from another package
+        b.callMethod();
+        b.methodPublic();
+
+    }
+}
