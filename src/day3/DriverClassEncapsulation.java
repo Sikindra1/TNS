4d89ce40b28061f58fd81c448bdaa9a703c69b7e
sikindra <yadavsikindra4078@gmail.com>
2023-04-21 21:28:42 +0530

    day 3 Java program solve

---
+++ src/day3/DriverClassEncapsulation.java
@@ -0,0 +1,11 @@
+package day3;
+
+public class DriverClassEncapsulation {
+    public static void main(String[] args) {
+        BaseClassEncapsulation obj=new BaseClassEncapsulation();
+        obj.setName("sikindra kumar yadav");
+        obj.setSerialNo(15);
+        obj.setAge(22);
+        System.out.println(obj);
+    }
+}
